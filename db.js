7e5bf48fa9bf42a79b23e7e85ecea51a7332563e
soufiane hamdach <112141817+soufianehamdach28@users.noreply.github.com>
2025-11-17 11:08:24 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 // db.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('bdbyja22pgxfilp9iudf', 'u8cvp6arz0w5ioq8', 'WbB7ndYElZw30MVBnzFX', {
-    host: 'bdbyja22pgxfilp9iudf-mysql.services.clever-cloud.com',
+const sequelize = new Sequelize('byeopdyaws8t2gu0oebi', 'u8cvp6arz0w5ioq8', 'WbB7ndYElZw30MVBnzFX', {
+    host: 'byeopdyaws8t2gu0oebi-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     logging: false,
 });
